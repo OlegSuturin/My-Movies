eac899be9142e1387f5f235d95c12475dcfb6b53
Oleg <oliverst@yandex.ru>
2020-11-14 17:23:19 +0300

    слушатели событий на RecyclerView (нажатие + подгрузка данных по достижению конца списка)

--- app/src/main/java/com/example/mymovies/MainActivity.java
+++ app/src/main/java/com/example/mymovies/MainActivity.java
@@ -2,11 +2,13 @@ package com.example.mymovies;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.mymovies.data.Movie;
@@ -15,11 +17,14 @@ import com.example.mymovies.utils.NetworkUtils;
 
 import org.json.JSONObject;
 
-import java.net.URL;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerViewPosters;
+    private Switch switchSort;
+    private TextView textViewPopilarity;
+    private TextView textViewTopRated;
+
     private MovieAdapter movieAdapter;
     private JSONObject jsonObject;
     private ArrayList<Movie> movies;
@@ -29,18 +34,71 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
+        switchSort = findViewById(R.id.switchSort);
         recyclerViewPosters = findViewById(R.id.recyclerViewPosters);
+        textViewPopilarity = findViewById(R.id.textViewPopilarity);
+        textViewTopRated = findViewById(R.id.textViewTopRated);
+
         movieAdapter = new MovieAdapter();
+        recyclerViewPosters.setLayoutManager(new GridLayoutManager(this, 2));   //установили отображение сеткой 2
+        recyclerViewPosters.setAdapter(movieAdapter);           //передали адаптер в RevyclerView
+        switchSort.setChecked(true);  // установили в сортировку по рейтингу - Слушатель не срабатывает т.к. он определен ниже.
 
-        recyclerViewPosters.setLayoutManager(new GridLayoutManager(this, 2));
+        //создали слушателя на свича
+        switchSort.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                setMethodOfSort(isChecked);
+            }
+        });
+        switchSort.setChecked(false);
 
-        jsonObject = NetworkUtils.getJSONFromNetwork(NetworkUtils.POPULARITY, 1);
-        movies = JSONUtils.getMoviesFromJSON(jsonObject);
-        movieAdapter.adddMovies(movies);
+            movieAdapter.setOnPosterClickListener(new MovieAdapter.OnPosterClickListener() {     //СОЗДАЛИ Слушатель - срабатывает на нажатие на Постер - элемент RecycleView
+                @Override
+                public void onPosterClick(int position) {
+                    Toast.makeText(MainActivity.this, "" + position, Toast.LENGTH_SHORT).show();
+                }
+            });
 
-        recyclerViewPosters.setAdapter(movieAdapter);
+          movieAdapter.setOnReachEndListener(new MovieAdapter.OnReachEndListener() {            //СОЗДАЛИ Слушатель - срабатывает на достижение конца списка - для подгрузки данных
+              @Override
+              public void onReachEnd() {
+                  Toast.makeText(MainActivity.this, "Конец списка", Toast.LENGTH_SHORT).show();
 
+              }
+          });
+
+
+
+
+    } // end of onCreate()
+
+    public void onClickTextViewPopilarity(View view) {
+        switchSort.setChecked(false);
     }
+
+    public void onClicktextViewTopRated(View view) {
+        switchSort.setChecked(true);
+    }
+
+    //Метод - установка типа сортировки и получение соответствующих данных
+    private void setMethodOfSort(boolean isToprated) {
+        int methodOfSort;
+        if (isToprated) {
+            methodOfSort = NetworkUtils.TOP_RATED;
+            textViewTopRated.setTextColor(getResources().getColor(R.color.teal_200));
+            textViewPopilarity.setTextColor(getResources().getColor(R.color.white));
+        } else {
+            methodOfSort = NetworkUtils.POPULARITY;
+            textViewPopilarity.setTextColor(getResources().getColor(R.color.teal_200));
+            textViewTopRated.setTextColor(getResources().getColor(R.color.white));
+        }
+
+
+        jsonObject = NetworkUtils.getJSONFromNetwork(methodOfSort, 1);
+        movies = JSONUtils.getMoviesFromJSON(jsonObject);
+        movieAdapter.setMovies(movies);
+    }
+
+
 }
--- app/src/main/java/com/example/mymovies/MovieAdapter.java
+++ app/src/main/java/com/example/mymovies/MovieAdapter.java
@@ -19,6 +19,8 @@ import java.util.ArrayList;
 //Адаптер для RecyclerView
 public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHolder> {
     ArrayList<Movie> movies;
+    OnPosterClickListener onPosterClickListener;   //объект ИНТЕРФЕЙСНОГО ТИПА слушателя на нажатие
+    OnReachEndListener onReachEndListener;         //объект ИНТЕРФЕЙСНОГО ТИПА слушателя на достижение конца списка
 
     public MovieAdapter() {
         movies = new ArrayList<>();   //создали массив
@@ -29,8 +31,8 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHol
         notifyDataSetChanged();  //всегда оповещаем адаптер об изменении массива
     }
 
-        //МЕТОД - когда будем листать список фильмов, будем добавлять (погружать) их в этот же массив, а не заменять старый массив новым
-    public void adddMovies(ArrayList<Movie> movies){
+    //МЕТОД - когда будем листать список фильмов, будем добавлять (погружать) их в этот же массив, а не заменять старый массив новым
+    public void addMovies(ArrayList<Movie> movies) {
         this.movies.addAll(movies);
         notifyDataSetChanged();   //всегда оповещаем адаптер об изменении массива
     }
@@ -39,7 +41,27 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHol
         return movies;
     }
 
+    //создаем СЛУШАТЕЛЯ на нажатие на картинку (элемент RecyclerView)
+    //ИНТЕРФЕЙС который содержит один метод
+    interface OnPosterClickListener {
+        void onPosterClick(int position);
+    }
 
+    // + сеттер на слушателя
+    public void setOnPosterClickListener(OnPosterClickListener onPosterClickListener) {
+        this.onPosterClickListener = onPosterClickListener;
+    }
+
+    //создаем СЛУШАТЕЛЯ на определение - достижения просмотра последнего элемента -  для подгрузки остальных данных из интернета и размещения их на RecycleView
+    interface OnReachEndListener {
+        void onReachEnd();        //метод вызываем при достижении конца листа - размещаем его в методе onBindViewHolder
+        //т.к. онс связывает Холдер с ПОЗИЦИЕЙ элемента
+    }
+
+    // + сеттер на слушателя
+    public void setOnReachEndListener(OnReachEndListener onReachEndListener) {
+        this.onReachEndListener = onReachEndListener;
+    }
 
     @NonNull
     @Override                   //передаем макет XML холдеру при создании холдера
@@ -50,9 +72,12 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHol
 
     @Override
     public void onBindViewHolder(@NonNull MovieViewHolder holder, int position) {
-        Movie movie = movies.get(position);   //получили объект из массива № position
+        if (position > movies.size() - 4 && onReachEndListener != null) {        //Определяем что до конца списка осталось 4 постера, что бы начать загрузку данных ЗАВРАНЕЕ
+            onReachEndListener.onReachEnd();    // вызываем метод интерфейса
+        }
 
-                                // ИСПОЛЬЗУЕМ БИБЛИОТЕКУ picasso для загрузки избражения из сети
+        Movie movie = movies.get(position);   //получили объект из массива № position
+        // ИСПОЛЬЗУЕМ БИБЛИОТЕКУ picasso для загрузки избражения из сети
         Picasso.get().load(movie.getPosterPath()).into(holder.imageViewSmallPoster);
     }
 
@@ -61,11 +86,21 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHol
         return movies.size();
     }
 
-    class MovieViewHolder extends RecyclerView.ViewHolder{              //Холдер - держатель View
-            private ImageView imageViewSmallPoster;
-        public MovieViewHolder(@NonNull View itemView) {
+    class MovieViewHolder extends RecyclerView.ViewHolder {              //Холдер - держатель View
+        private ImageView imageViewSmallPoster;
+
+        public MovieViewHolder(@NonNull View itemView) {           //КОНСТРУКТОР холдера
             super(itemView);
             imageViewSmallPoster = itemView.findViewById(R.id.imageViewSmallPoster);
+
+            itemView.setOnClickListener(new View.OnClickListener() {        //РАЗМЕЩЕНИЕ слушателя на нажатие - в конструкторе ViewHolder
+                @Override
+                public void onClick(View v) {
+                    if (onPosterClickListener != null) {
+                        onPosterClickListener.onPosterClick(getAdapterPosition());   // вызываем метод интерфейса
+                    }
+                }
+            });
 
 
         }
